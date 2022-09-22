e6d524fb592ac89216c7c59d6d265009f78b5f30
wen <3306225831@qq.com>
2022-09-22 22:42:54 +0800

    commit-third

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello branch-test");
+        System.out.println("hello git");
     }
 }
